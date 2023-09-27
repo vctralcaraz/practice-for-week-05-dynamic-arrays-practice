0e31c4d8de0d059138900001e039af981b1cce3d
Victor Alcaraz <vctralcaraz@gmail.com>
2023-09-26 18:58:08 -0700

    complete dynamic arrays

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,19 +1,29 @@
 class DynamicArray {
 
-  constructor(defaultSize=4) {
+  constructor(defaultSize = 4) {
 
     // Your code here
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
 
   }
 
   read(index) {
 
     // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
 
     // Your code here
+    for(let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+
+    this.data[0] = val;
+    this.length += 1;
   }
 
 }
